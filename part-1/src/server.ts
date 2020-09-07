a2151f3264b88eda8e9a2d7e539b3cda4f388fe0
Pedro Nobrega <pedro.nobrega@gmail.com>
2020-09-07 12:35:00 -0300

    fix: minor enhancements

--- part-1/src/server.ts
+++ part-1/src/server.ts
@@ -11,13 +11,6 @@ import Cluster from './cluster';
 // eslint-disable-next-line import/first
 import Worker from './worker';
 
-// THIS IS NOT THE BETTER WAY TO CLUSTER A NODE SERVER.
-// OTHER OPTIONS ARE MORE VIABLE, SUCH AS IPTABLE OR NGINX.
-// WHY DID I NOT USE IPTABLE? IT HAS TO BE CONFIGURED MANUALLY.
-// TODO - ENHANCEMENT => APPLY NGINX
-
-// setupExpress({});
-
 const setup = () => (cluster.isMaster ? new Cluster() : new Worker());
 
 setup();
